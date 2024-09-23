16c61fadc38ee9556cd8f4115e6244065fdf5c49
Caleb Durenberger <calebduren@users.noreply.github.com>
2024-09-23 15:44:59 -0700

    Remove some tailwind classes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,14 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-<header className="header mb-12 flex flex-col gap-4 items-center">
+<header className="header">
     <div className="logo-container">
         <Logo />
     </div>
-    <div className="flex flex-col items-center m-0 max-w-full">
+    <div className="header-text">
         <h1>Loud</h1>
-        <h2 className="subheader text-gray-700 text-2xl mb-1 font-medium text-center">Find your next favorite record</h2>
-        <h3 className="text-lg text-gray-500 text-center">Loud is a Spotify new releases app with filters</h3>
+        <h2>Find your next favorite record</h2>
+        <h3>Loud is an app to filter Spotify new releases</h3>
     </div>
     </header>
   );
