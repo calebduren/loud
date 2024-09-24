d53a3fe8f5434fee1be40337f69a133eac6f1e04
Caleb Durenberger <calebduren@users.noreply.github.com>
2024-09-24 09:23:00 -0700

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner: React.FC = () => {
   return (
     <div className="banner">
-      <p>Due to a <a className="underline" href="https://community.spotify.com/t5/Spotify-for-Developers/Spotify-API-returns-403-for-new-releases/td-p/10886889" target="_blank" rel="noopener noreferrer">known issue</a> with Spotify&rsquo;s API, we are currently unable to fetch releases newer than April&nbsp;2024.</p>
+      <p>Due to a known issue with Spotify&rsquo;s API, we are currently unable to fetch releases newer than April&nbsp;2024.</p>
     </div>
   );
 };
