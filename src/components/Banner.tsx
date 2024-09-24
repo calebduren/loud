284c903206d8c9aff05b489149fb5caf29c995e5
Caleb Durenberger <calebduren@users.noreply.github.com>
2024-09-24 09:30:17 -0700

    Update Banner.tsx

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Banner: React.FC = () => {
   return (
     <div className="banner">
-      <p>Due to a known issue with Spotify&rsquo;s API, we are currently unable to fetch all new&nbps;releases.</p>
+      <p>Due to a known issue with Spotify&rsquo;s API, we are currently unable to fetch all new&nbsp;releases.</p>
     </div>
   );
 };
