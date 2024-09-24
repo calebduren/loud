abad7d07972629b949d84f2db29d384c73f29cdf
Caleb Durenberger <calebduren@users.noreply.github.com>
2024-09-24 15:34:12 -0700

    Format

--- src/components/ReleaseCard.tsx
+++ src/components/ReleaseCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 interface Release {
   id: string;
@@ -11,30 +11,54 @@ interface Release {
 }
 
 const ReleaseCard: React.FC<{ release: Release }> = ({ release }) => (
-    <a href={`https://open.spotify.com/album/${release.id}`} target="_blank" rel="noopener noreferrer" className="release-card" title="Open in Spotify">
-        <div className="release-card-top">
-            <img className="release-image" src={release.image} alt="Release cover" width="100%" height="100%"/>
-            <div className="gradient-overlay"></div>
-            <div className="release-info">
-                <div className="tag-container">
-                    <span className="tag tag-type font-semibold">{release.type === "ep" ? "EP" : release.type.charAt(0).toUpperCase() + release.type.slice(1)}</span>
-                    <div className="open-in-spotify tag">Open in Spotify ↗</div>
-                </div>
-                <div className="release-info-container">
-                    <h3 className="artist-name">{release.artist}</h3>
-                    <p className="album-name">{release.name}</p>
-                </div>
-            </div>
+  <a
+    href={`https://open.spotify.com/album/${release.id}`}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="release-card"
+    title="Open in Spotify"
+  >
+    <div className="release-card-top">
+      <img
+        className="release-image"
+        src={release.image}
+        alt="Release cover"
+        width="100%"
+        height="100%"
+      />
+      <div className="gradient-overlay"></div>
+      <div className="release-info">
+        <div className="tag-container">
+          <span className="tag tag-type font-semibold">
+            {release.type === "ep"
+              ? "EP"
+              : release.type.charAt(0).toUpperCase() + release.type.slice(1)}
+          </span>
+          <div className="open-in-spotify tag">Open in Spotify ↗</div>
         </div>
-        <div className="release-card-bottom">
-            <div className="release-card-bottom-content">
-                <div className="release-date release-meta-item"><div className="label">Released</div> {new Date(release.releaseDate).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}</div>
-                <div className="total-tracks release-meta-item"><div className="label">Tracks</div> {release.totalTracks}</div>
-                <div className="touch-only faux-link">Open in Spotify ↗</div>
-            </div>
+        <div className="release-info-container">
+          <h3 className="artist-name">{release.artist}</h3>
+          <p className="album-name">{release.name}</p>
         </div>
-    </a>
-  );
+      </div>
+    </div>
+    <div className="release-card-bottom">
+      <div className="release-card-bottom-content">
+        <div className="release-date release-meta-item">
+          <div className="label">Released</div>{" "}
+          {new Date(release.releaseDate).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          })}
+        </div>
+        <div className="total-tracks release-meta-item">
+          <div className="label">Tracks</div> {release.totalTracks}
+        </div>
+        <div className="touch-only faux-link">Open in Spotify ↗</div>
+      </div>
+    </div>
+  </a>
+);
 
 export default ReleaseCard;
-
