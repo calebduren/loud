76388031dc54c74112dfc60d139275cb970825a5
Caleb Durenberger <calebduren@users.noreply.github.com>
2024-09-24 16:08:34 -0700

    Update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import Logo from "./Logo";
 import React from "react";
-import QuestionMark from "../assets/icons/QuestionMark.tsx";
+import Asterisk from "../assets/icons/Asterisk.tsx";
 
 const Header: React.FC = () => {
   return (
@@ -11,8 +11,8 @@ const Header: React.FC = () => {
       <div className="header-text">
         <h1>Loud</h1>
         <h2>Find your next obsession</h2>
-        <div className="flex items-center gap-1">
-          <QuestionMark />
+        <div className="loud-description">
+          <Asterisk />
           <h3>Loud is an app to filter Spotify new releases</h3>
         </div>
       </div>
