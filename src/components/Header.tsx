c806effdc5878d8c6e256446ee97e0b7d0cff1b5
Caleb Durenberger <calebduren@users.noreply.github.com>
2024-09-23 17:05:49 -0700

    Icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import Logo from "./Logo";
 import React from "react";
-import QuestionMark from "../assets/icons/QuestionMark.svg";
+import QuestionMark from "../assets/icons/QuestionMark.tsx";
 
 const Header: React.FC = () => {
   return (
@@ -10,11 +10,11 @@ const Header: React.FC = () => {
     </div>
     <div className="header-text">
         <h1>Loud</h1>
-        <h2>Find your next favorite&nbsp;record</h2>
-        <div className="flex items-center gap-1"><img width={14} height={14} src={QuestionMark} alt="Question mark" /> <h3>Loud is an app to filter Spotify new&nbsp;releases</h3></div>
+        <h2>Find your next favorite record</h2>
+        <div className="flex items-center gap-1"><QuestionMark /> <h3>Loud is an app to filter Spotify new releases</h3></div>
     </div>
     </header>
   );
-};
+}
 
 export default Header;
